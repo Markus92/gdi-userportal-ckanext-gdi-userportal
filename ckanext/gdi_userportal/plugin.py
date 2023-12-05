7abe977be457601a60a1824cddef9f942b048a28
Younès Adem <63497031+admy7@users.noreply.github.com>
2023-12-05 14:19:53 +0100

    Feature/ckan UI (#3)
    * add font and colors
    
    * add more styles
    
    * fix vertical alignment

--- ckanext/gdi_userportal/plugin.py
+++ ckanext/gdi_userportal/plugin.py
@@ -9,9 +9,9 @@ class GdiUserPortalPlugin(plugins.SingletonPlugin):
     # IConfigurer
 
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('assets', 'ckanext-gdi-userportal')
+        toolkit.add_template_directory(config_, "templates")
+        toolkit.add_public_directory(config_, "public")
+        toolkit.add_resource("assets", "ckanext-gdi-userportal")
 
     # IFacets
 
