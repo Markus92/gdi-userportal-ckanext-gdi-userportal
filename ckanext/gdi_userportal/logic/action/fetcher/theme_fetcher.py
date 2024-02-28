c19f5708a7e299dbdc9ad6181fb45392fe4789b0
Hans-Christian <hcvanderwerf@gmail.com>
2024-02-28 16:08:13 +0100

    fix(theme-fetcher): handle empty iterable in theme reduction process (#36)
    This commit addresses an issue where the `reduce` function in the `ThemeFetcher` class would throw an error when called with an empty iterable. By providing an initial value (an empty list), we ensure that the function can handle empty datasets gracefully, preventing runtime errors.
    
    Closes #35
    
    Co-authored-by: Hans-Chrstian <hanschristian.vanderwerf@health-ri.nl>

--- ckanext/gdi_userportal/logic/action/fetcher/theme_fetcher.py
+++ ckanext/gdi_userportal/logic/action/fetcher/theme_fetcher.py
@@ -1,16 +1,15 @@
 # SPDX-FileCopyrightText: 2024 PNED G.I.E.
-#
+# SPDX-FileContributor: Stichting Health-RI
 # SPDX-License-Identifier: Apache-2.0
 
 from functools import reduce
 
 from ckanext.gdi_userportal.logic.action.fetcher.prop_fetcher import PropFetcher
 
-
 class ThemeFetcher(PropFetcher):
     def _get_batched_prop_values(self, batched_datasets) -> list[str]:
         themes = super()._get_batched_prop_values(batched_datasets)
-        themes = list(set(reduce(lambda themes1, themes2: themes1 + themes2, themes)))
+        themes = list(set(reduce(lambda themes1, themes2: themes1 + themes2, themes, [])))
         return themes
 
     @property
