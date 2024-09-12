bc62bc437a8e1a444c5f8702fbc70113701905de
Younès Adem <younes.adem@lnds.lu>
2024-09-12 14:43:24 +0200

    fix: licenses

--- ckanext/gdi_userportal/logic/action/post.py
+++ ckanext/gdi_userportal/logic/action/post.py
@@ -1,3 +1,8 @@
+# SPDX-FileCopyrightText: 2024 Stichting Health-RI
+# SPDX-FileContributor: PNED G.I.E.
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from ckan.plugins import toolkit
 from ckanext.gdi_userportal.logic.action.translation_utils import (
     collect_values_to_translate,
