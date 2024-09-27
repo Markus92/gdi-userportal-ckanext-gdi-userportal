64725fd8f4c29d8e31eb3fe95c31fceccb73e6b5
Hans-Christian <hcvanderwerf@gmail.com>
2024-09-27 13:19:43 +0200

    feat: Upgrade to DCAT AP 3 (#99)
    - Remove `before index` JSON encoding
    - Update Solr
    - Update schema and presets according to DCAT AP 3
    
    Co-authored-by: Hans-Chrstian <hanschristian.vanderwerf@health-ri.nl>

--- ckanext/gdi_userportal/plugin.py
+++ ckanext/gdi_userportal/plugin.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import json
-
+import logging 
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from ckanext.gdi_userportal.logic.action.get import (
@@ -105,40 +105,7 @@ class GdiUserPortalPlugin(plugins.SingletonPlugin):
     def after_dataset_search(self, search_results, search_params):
         return search_results
 
-    def _parse_to_array(self, data_dict, field):
-        extras_field = f"extras_{field}"
-        if data_dict.get(extras_field):
-            try:
-                data_dict[field] = json.loads(data_dict[extras_field])
-            except json.JSONDecodeError:
-                data_dict[field] = data_dict[extras_field]
-            del data_dict[extras_field]
-        return data_dict
-
-    def before_dataset_index(self, data_dict):
-        for field in self._dcatap_fields_to_normalize:
-            data_dict = self._parse_to_array(data_dict, field)
-
-        context = {"model": model, "session": model.Session}
-        dataset_dict = {"type": "dataset"}
-        schema = toolkit.get_action("scheming_dataset_schema_show")(
-            context, dataset_dict
-        )
-
-        repeating_fields = [
-            x.get("field_name")
-            for x in schema.get("dataset_fields", [])
-            if "repeating_subfields" in x
-        ]
-        for field in repeating_fields:
-            try:
-                data_dict[field] = [json.dumps(x) for x in data_dict[field]]
-            except (json.JSONDecodeError, KeyError):
-                pass
-
-        if data_dict.get("res_format"):
-            data_dict["res_format"] = list(dict.fromkeys(data_dict.get("res_format")))
-
+    def before_dataset_index(self, data_dict):       
         return data_dict
 
     def before_dataset_view(self, pkg_dict):
